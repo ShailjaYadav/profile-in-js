e1b4f1c53cdb06b1e15373aafc034c54b86d53d4
ShailjaYadav <123499484+ShailjaYadav@users.noreply.github.com>
2024-12-25 21:33:23 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,29 @@
+console.log("Welcome to Apna College");
+console.log("Shailja Yadav");
+console.log("I love JavaScript");
+fullName="Tony stark";
+console.log(fullName);
+const student={
+    fullName:"Rahul Kumar",
+    age:20,
+    cgpa:8.2,
+    isPass: true,
+};
+student["name"]="Rahul Sharma";
+console.log(student["name"]);
+const product={
+    title:"Ball Pen",
+    rating: 4,
+    offer: 5,
+    price:270,
+};
+console.log(product);
+
+const profile={
+    userName:"Shailja",
+    post:195,
+    followers:10,
+    following:4,
+    email:"shailjay943@gmail.com",
+};
+console.log(profile);
